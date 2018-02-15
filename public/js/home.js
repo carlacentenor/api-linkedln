73eac953ce3419273311b7545e3831926ea4a354
AlejandraCP <alejandracp710@gmail.com>
2018-02-15 04:52:07 -0500

    Adding post

--- public/js/home.js
+++ public/js/home.js
@@ -1,9 +1,13 @@
-let $firstnameUser = $('#firstname');
-let $lastnameUser = $('#lastname');
-let $headlineUser = $('#headline');
+let $firstnameUser = $('.firstname');
+let $lastnameUser = $('.lastname');
+let $headlineUser = $('.headline');
 let $connectionsUser = $('#connections');
 let $imgUser = $('#photo-user');
 let $imgPost = $('.img-post');
+let $btnPost = $('.bg-info');
+let $text = $('#text');
+let $contPost = $('.container-post');
+$btnPost.disabled = true;
 
 function getInfoUser() {
   let url = localStorage.pictureUrl;
@@ -21,3 +25,18 @@ $('#tab1').on('click', function(event) {
   event.preventDefault();
   $(this).tab('show');
 });
+
+$btnPost.on('click', post);
+
+function post(event) {
+  let textPost = $text.val();
+  $contPost.append(`<div class="row cont-post">
+  <div class="col-4">
+  <img  alt="photo" class="img-post rounded-circle img-fluid" src=${localStorage.pictureUrl}>
+  <p>${localStorage.firstName}</p>
+  </div>
+  <div class="col-8">
+  <p>${textPost}</p>
+  </div>
+  </div>`);
+};
